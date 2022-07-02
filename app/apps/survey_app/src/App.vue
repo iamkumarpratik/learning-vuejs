8800c8c642f27e258138d8b15d1d6059de96472e
Kumar Pratik <kpratik@greenstreet.com>
2022-07-02 21:30:52 +0530

    mono repo commit

---
+++ app/apps/survey_app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #a6b0b6;
+}
+
+nav a.router-link-exact-active {
+  color: #4856e8;
+}
+</style>
---
+++ app/apps/survey_app/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-mono">{{ msg }}</h1>
+    <p class="mt-8 text-xl">
+    To take the survey click on the below button. We are excited to learn about you! If you would like to know about us,
+      click on the About tab above on the Navbar.
+    </p>
+  </div>
+  <EditButton />
+</template>
+
+<script>
+import '../assets/tailwind.css'
+import EditButton from "@/components/EditButton";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    EditButton,
+  }
+}
+</script>
+
